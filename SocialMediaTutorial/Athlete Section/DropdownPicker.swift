b5fad6df6a6926539796e0bea415c7996bab04e8
FRCroboSam <samuelwang787@gmail.com>
2024-03-22 18:54:35 -0700

    added dropdown picker

--- SocialMediaTutorial/Athlete Section/DropdownPicker.swift
+++ SocialMediaTutorial/Athlete Section/DropdownPicker.swift
@@ -15,68 +15,78 @@ struct DropdownPicker: View {
     let sports = ["Swimming", "Tennis", "Baseball", "Football", "Lacrosse", "Badminton", "Soccer", "Rugby",
                                    "Basketball", "Pickleball", "Cross Country", "Track and Field"]
     var body: some View{
-        ZStack{
-            VStack{
-                VStack(alignment: .leading){
-                    HStack{
-                        Text("Select a Sport: ")
-                        Spacer()
-                        Image(systemName: "chevron.down")
-                            .font(.subheadline)
-                            .foregroundStyle(.gray)
-                            .rotationEffect(.degrees(isExpanded ? -180 : 0))
-                    }
-                    .onTapGesture {
-                        isExpanded = !isExpanded
-                    }
-                    .frame(maxWidth: .infinity, alignment: .leading) // Align text within its frame to the leading edge
-
-                }
-                .frame(width: 3/4 * 3/4 * deviceWidth)
-                .padding()
-                .background(Color.orange)
-                .cornerRadius(15)
-                VStack(alignment: .leading){
-                    HStack{
-                        Text("Select a Sport: ")
-                        Spacer()
-                        Image(systemName: "chevron.down")
-                            .font(.subheadline)
-                            .foregroundStyle(.gray)
-                            .rotationEffect(.degrees(isExpanded ? -180 : 0))
-                    }
-                    .frame(maxWidth: .infinity, alignment: .leading) // Align text within its frame to the leading edge
-
-                }
-                .frame(width: 3/4 * 3/4 * deviceWidth)
-                .padding()
-                .background(Color.orange)
-                .cornerRadius(15)
-            }
-
-            
-            if(isExpanded){
-                ScrollView{
+        VStack{
+            Text("HELLO")
+                VStack{
                     VStack(alignment: .leading){
-                        ForEach(sports, id: \.self) {
-                            Text($0)
+                        HStack{
+                            Text("Select a Sport: ")
                                 .font(.system(size: 20))
-                                .foregroundColor(.black)
-                                .multilineTextAlignment(.leading) // Align text to the left
-                                .frame(maxWidth: .infinity, alignment: .leading) // Align text within its frame to the leading edge
-                            Divider()
+                            Spacer()
+                            Image(systemName: "chevron.down")
+                                .font(.subheadline)
+                                .foregroundStyle(.gray)
+                                .rotationEffect(.degrees(isExpanded ? -180 : 0))
+                        }
+                        .onTapGesture {
+                            isExpanded = !isExpanded
+                        }
+                        .frame(maxWidth: .infinity, alignment: .leading) // Align text within its frame to the leading edge
+                        
+                    }
+                    .frame(width: 3/4 * 3/4 * deviceWidth)
+                    .padding()
+                    .background(Color.orange)
+                    .clipShape(
+                        .rect(
+                            topLeadingRadius: 20,
+                            bottomLeadingRadius: isExpanded ? 0 : 20,
+                            bottomTrailingRadius: isExpanded ? 0 : 20,
+                            topTrailingRadius: 20
+                        )
+                    )
+                }
+                .overlay{
+                    
+                    
+                    ZStack{
+                        
+                        if(isExpanded){
+                            VStack{
+                                ScrollView{
+                                    VStack(alignment: .leading){
+                                        ForEach(sports, id: \.self) {
+                                            Text($0)
+                                                .font(.system(size: 20))
+                                                .foregroundColor(.black)
+                                                .multilineTextAlignment(.leading) // Align text to the left
+                                                .frame(maxWidth: .infinity, alignment: .leading) // Align text within its frame to the leading edge
+                                            Divider()
+                                            
+                                        }
+                                    }
+                                }
+                            }
+                            .frame(width: 3/4 * 3/4 * deviceWidth, height: 200)
+                            .padding()
+                            .background(Color.orange)
+                            .clipShape(
+                                .rect(
+                                    topLeadingRadius: 0,
+                                    bottomLeadingRadius: 20,
+                                    bottomTrailingRadius: 20,
+                                    topTrailingRadius: 0
+                                )
+                            )
+                            
                             
                         }
                     }
-                }
-                .frame(width: 3/4 * 3/4 * deviceWidth, height: 200)
-                .padding()
-                .background(Color.orange)
-                .cornerRadius(15)
-                
-                .offset(x: 0, y: 150)
+                    .layoutPriority(10)
+                    .offset(x: 0, y: 140)
+                    .zIndex(200)
 
-            }
+                }
         }
 
     }
--- SocialMediaTutorial/Athlete Section/FilterView.swift
+++ SocialMediaTutorial/Athlete Section/FilterView.swift
@@ -24,63 +24,34 @@ struct FilterView: View {
     @State private var isExpanded: Bool = false
     
     var body: some View {
-        VStack{
-            Text("Apply Filters")
-                .font(.system(size: 40))
-            VStack(alignment: .leading){
-                HStack{
-                    Text("Select a Sport: ")
-                    Spacer()
-                    Image(systemName: "chevron.down")
-                        .font(.subheadline)
-                        .foregroundStyle(.gray)
-                        .rotationEffect(.degrees(isExpanded ? -180 : 0))
-                }
-                .onTapGesture {
-                    isExpanded = !isExpanded
-                }
-                .frame(maxWidth: .infinity, alignment: .leading) // Align text within its frame to the leading edge
-
-            }
-            .frame(width: 3/4 * 3/4 * deviceWidth)
-            .padding()
-            .background(Color.orange)
-            .cornerRadius(15)
-            if(isExpanded){
-                ScrollView{
-                    ForEach(sports, id: \.self) {
-                        Text($0)
-                            .font(.system(size: 20))
-                            .foregroundColor(.black)
-                            .multilineTextAlignment(.leading) // Align text to the left
-                            .frame(maxWidth: .infinity, alignment: .leading) // Align text within its frame to the leading edge
-                        Divider()
-
+            VStack{
+                Text("Apply Filters")
+                    .font(.system(size: 40))
+                DropdownPicker()
+                    .zIndex(500)
+                
+                VStack(alignment: .leading){
+                    HStack{
+                        Text("Choose a year: ")
+                        Spacer()
+                        Image(systemName: "chevron.down")
+                            .font(.subheadline)
+                            .foregroundStyle(.gray)
+                            .rotationEffect(.degrees(isExpanded ? -180 : 0))
                     }
+                    .onTapGesture {
+                        isExpanded = !isExpanded
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading) // Align text within its frame to the leading edge
+                    
                 }
+                .frame(width: 3/4 * 3/4 * deviceWidth)
+                .padding()
+                .background(Color.orange)
+                .cornerRadius(15)
+                Spacer()
+                
             }
-            VStack(alignment: .leading){
-                HStack{
-                    Text("Choose a year: ")
-                    Spacer()
-                    Image(systemName: "chevron.down")
-                        .font(.subheadline)
-                        .foregroundStyle(.gray)
-                        .rotationEffect(.degrees(isExpanded ? -180 : 0))
-                }
-                .onTapGesture {
-                    isExpanded = !isExpanded
-                }
-                .frame(maxWidth: .infinity, alignment: .leading) // Align text within its frame to the leading edge
-
-            }
-            .frame(width: 3/4 * 3/4 * deviceWidth)
-            .padding()
-            .background(Color.orange)
-            .cornerRadius(15)
-
-        }
-
         .frame(width: 3/4 * deviceWidth, height: 1/2 * deviceHeight)
         .background(
             RoundedRectangle(cornerRadius: 10)
